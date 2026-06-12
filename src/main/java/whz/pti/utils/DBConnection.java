054109a5a359a1aabb0e6e565b48832bc0f1fb0e
sanzhar-berdikozhoev <breadydev@gmail.com>
2026-06-12 18:50:02 +0200

    added DB-Connection help object

---
+++ src/main/java/whz/pti/utils/DBConnection.java
@@ -0,0 +1,86 @@
+package whz.pti.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+
+public class DBConnection {
+    private static final Logger logger = Logger.getLogger(DBConnection.class.getName());
+
+    private static DBConnection instance;
+    private Connection connection;
+
+    // Config parameters
+    private String server;
+    private String port;
+    private String database;
+    private String username;
+    private String password;
+    private boolean encrypt;
+    private boolean trustServerCertificate;
+
+    private DBConnection() {
+        loadProperties();
+    }
+
+    public static synchronized DBConnection getInstance() {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    private void loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                logger.warning("config.properties file not found. Using default values.");
+                setDefaultProperties();
+            } else {
+                properties.load(input);
+                this.server = properties.getProperty("db.server", "localhost");
+                this.port = properties.getProperty("db.port", "1433");
+                this.database = properties.getProperty("db.database", "SmarthomeDB");
+                this.username = properties.getProperty("db.username", "sa");
+                this.password = properties.getProperty("db.password", "");
+                this.encrypt = Boolean.parseBoolean(properties.getProperty("db.encrypt", "false"));
+                this.trustServerCertificate = Boolean.parseBoolean(properties.getProperty("db.trustServerCertificate", "true"));
+            }
+        } catch (IOException e) {
+            logger.warning("Error loading config.properties: " + e.getMessage());
+            setDefaultProperties();
+        }
+    }
+
+    private void setDefaultProperties() {
+        this.server = "localhost";
+        this.port = "1433";
+        this.database = "SmarthomeDB";
+        this.username = "sa";
+        this.password = "";
+        this.encrypt = false;
+        this.trustServerCertificate = true;
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = createConnection();
+        }
+        return connection;
+    }
+
+    private Connection createConnection() throws SQLException {
+        String connectionString = String.format(
+                "jdbc:sqlserver://%s:%s;databaseName=%s;encrypt=%b;trustServerCertificate=%b",
+                server, port, database, encrypt, trustServerCertificate
+        );
+
+        logger.info("Connecting to database: " + connectionString);
+        return DriverManager.getConnection(connectionString, username, password);
+    }
+}
