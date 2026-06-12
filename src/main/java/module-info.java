0ae6978b934fdfc712d54bf813cca16bf41858a4
2pizzzza <amit.kurbanov.123.com@gmail.com>
2026-06-12 19:52:35 +0200

    minor commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module whz.pti {
+    requires java.logging;
+    requires javafx.controls;
+    requires java.sql;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    exports whz.pti;
+}
